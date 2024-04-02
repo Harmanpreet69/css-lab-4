0d3178e15b4ad39e3e5465cdf74c27d987baf4d1
Harmanpreet69 <200567042@student.georgianc.on.ca>
2024-04-01 23:20:59 -0400

    Uploaded files

---
+++ js/tables.js
@@ -0,0 +1,13 @@
+// STEP 11: Call to tablesorter script here
+
+const table1 = document.querySelector("#standingsTable1");
+const t1 = new Tablesort(table1);
+
+const table2 = document.querySelector("#standingsTable2");
+const t2 = new Tablesort(table2);
+
+const table3 = document.querySelector("#standingsTable3");
+const t3 = new Tablesort(table3);
+
+const table4 = document.querySelector("#standingsTable4");
+const t4 = new Tablesort(table4);
